7a65b482305f57ec252c44c809c4a78f7fb7ad8a
nbond211 <bond.n@husky.neu.edu>
2017-06-04 18:29:45 -0400

    style album card component

--- src/components/album-grid/album-card/album-card.js
+++ src/components/album-grid/album-card/album-card.js
@@ -14,13 +14,13 @@ class AlbumCard extends Component {
     const {imageUrl, title, artist} = this.props;
 
     return (
-      <div className="uk-card uk-card-default uk-card-hover">
+      <div className="uk-card uk-card-default uk-card-hover album-card">
             <div className="uk-card-media-top">
                 <img src={imageUrl} alt={title}/>
             </div>
-            <div className="uk-card-body">
-                <h3 className="uk-card-title">{title}</h3>
-                <p>{artist}</p>
+            <div className="uk-card-body card-body">
+                <h3 className="uk-card-title album-title">{title}</h3>
+                <p className="album-artist">{artist}</p>
             </div>
         </div>
     );
